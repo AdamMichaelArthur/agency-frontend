e4c679636d4b13482eb3c99dbb7546e26af01ba5
Adam Arthur <adam.arthur@contentbounty.com>
2023-10-29 15:28:18 +0300

    Deployment Test

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Home = () => {
+const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>This is a deploy test.  Let's see if work.</p>
+      <p>
+        Legal Entity: inBrain, LLC.<br />
+        Address: 3916 N. Potsdam Ave, #3522, Sioux Falls, SD. 57104<br />
+        Phone: 619-798-6821<br />
+        <a href="https://wyobiz.wyo.gov/Business/FilingDetails.aspx?eFNum=052112065176246131084102034088026224103252115147">Registered In Wyoming</a>
+      </p>
+      <b>Our Brands</b><ul><li>hammerheadads.com</li><li>yourfullschool.com</li></ul>
     </div>
   );
 };
 
-export default Home;
+export default Contact;
